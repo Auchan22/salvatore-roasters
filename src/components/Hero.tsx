c41156afcff906269b5dce44dd177b1bf398e46e
Agustin <agustinsurila@gmail.com>
2023-01-31 15:09:47 -0300

    Initial Commit

---
+++ src/components/Hero.tsx
@@ -0,0 +1,12 @@
+import { Container } from '@chakra-ui/react';
+import React from 'react';
+
+const Hero = () => {
+  return (
+    <div>
+      <Container>as</Container>
+    </div>
+  );
+};
+
+export default Hero;
